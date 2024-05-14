758278721a6489c0f2f66029a79728584ddfe03a
Liam Mesarec <mesarec.liam@gmail.com>
2024-05-14 14:33:26 +0200

    feature: struct

--- src/parser/struct_.rs
+++ src/parser/struct_.rs
@@ -1,7 +1,6 @@
-use crate::parser::body;
 use crate::parser::operator;
 
-use crate::parser::{Node, ParseResult, ParserInfo, Error};
+use crate::parser::{Error, Node, ParseResult, ParserInfo};
 use crate::tokenizer::Token;
 
 ///struct {
@@ -22,27 +21,9 @@ pub fn struct_(parser_info: &mut ParserInfo) -> ParseResult {
 
     let mut struct_body = Node::new_box(&parser_info.current_token_info);
 
-    while parser_info.match_token(Token::Identifier) {
-        struct_body.children.push(Node::new_box(&parser_info.current_token_info));
-
-        if !parser_info.match_token(Token::Colon) {
-            //TODO ERROR
-            return Err(Error::MissingClosingBrackets(
-                parser_info.current_token_info.clone(),
-            ));
-        }
-
-        struct_body.children.push(Node::new_box(&parser_info.current_token_info));
-
-        if !parser_info.match_token(Token::Identifier) {
-            //TODO ERROR
-            return Err(Error::MissingClosingBrackets(
-                parser_info.current_token_info.clone(),
-            ));
-        }
-
-        struct_body.children.push(Node::new_box(&parser_info.current_token_info));
-    }
+    struct_body
+        .children
+        .push(var_definition_list(parser_info, struct_body.clone())?);
 
     if !parser_info.match_token(Token::RightBraces) {
         return Err(Error::MissingClosingBrackets(
@@ -50,9 +31,42 @@ pub fn struct_(parser_info: &mut ParserInfo) -> ParseResult {
         ));
     }
 
-    struct_body.children
+    struct_body
+        .children
         .push(Node::new_box(&parser_info.current_token_info));
 
     node.children.push(struct_body);
     Ok(node)
+}
+
+pub fn var_definition_list(parser_info: &mut ParserInfo, mut parent: Box<Node>) -> ParseResult {
+    while parser_info.match_token(Token::Identifier) {
+        let mut definition = Node::new_box(&parser_info.current_token_info);
+
+        if !parser_info.match_token(Token::Colon) {
+            return Err(Error::MissingType(
+                parser_info.current_token_info.clone(),
+                parser_info.last_n_token_lexemes(3),
+            ));
+        }
+
+        definition
+            .children
+            .push(Node::new_box(&parser_info.current_token_info));
+
+        if !parser_info.match_token(Token::Identifier) {
+            return Err(Error::MissingType(
+                parser_info.current_token_info.clone(),
+                parser_info.last_n_token_lexemes(3),
+            ));
+        }
+
+        definition
+            .children
+            .push(Node::new_box(&parser_info.current_token_info));
+
+        parent.children.push(definition)
+    }
+
+    Ok(parent)
 }
