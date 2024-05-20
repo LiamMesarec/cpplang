05a60da107a277f03b9e1fcf968203dc62fb30ea
Liam Mesarec <mesarec.liam@gmail.com>
2024-05-20 17:55:49 +0200

    feature: to cpp

--- src/evaluator/to_cpp.rs
+++ src/evaluator/to_cpp.rs
@@ -1,1 +1,12 @@
- 
+use std::collections::HashMap;
+
+pub fn init_types() -> HashMap<String, String> {
+    let mut hashmap = HashMap::new();
+    hashmap.insert("".to_string(), "".to_string());
+
+    hashmap
+}
+
+pub fn translate_type(str_type: &str) {
+
+}
